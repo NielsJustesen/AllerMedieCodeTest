1f2bfb9a703f833b59e9fe10ab283c2753f0abf0
NielsJustesen <niels449@hotmail.com>
2023-09-15 17:13:12 +0200

    Components added

---
+++ components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="mt-4 w-100 bg-red-600 mb-4">
+    <h1 class="text-5xl font-bold text-white pl-1 pt-1 pb-1">SKÆBNER</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <Tutorial/>
+  <Header />
 </template>
 
 <script>
+import Header from "../components/Header.vue";
 export default {
-  name: 'IndexPage'
-}
+  components: {
+    Header,
+  },
+  name: "IndexPage",
+};
 </script>
