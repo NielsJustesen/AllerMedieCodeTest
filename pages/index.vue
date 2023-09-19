16c8d4362269f36fb985e20ea5f98326ede0b7b8
NielsJustesen <niels449@hotmail.com>
2023-09-19 23:18:09 +0200

    pb was a cheat

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full flex justify-center bg-sand-200 pb-20">
+  <div class="w-full flex justify-center bg-sand-200 pb-5">
     <div class="flex flex-col w-1190">
       <Header />
       <div class="w-full lg:flex">
